3c33f47c97729b42a7800c7353b097055c17ec69
LilDre7 <alvaroaburto71@gmail.com>
2024-10-31 12:40:02 -0600

    :zap: Improve performace

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,23 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+export const validateString = (
+  value: unknown,
+  maxLength: number
+): value is string => {
+  if (!value || typeof value !== "string" || value.length > maxLength) {
+    return false;
+  }
 
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
+  return true;
+};
+export const getErrorMessage = (error: unknown): string => {
+  let message: string;
+  if (error instanceof Error) {
+    message = error.message;
+  } else if (error && typeof error === "object" && "message" in error) {
+    message = String(error.message);
+  } else if (typeof error === "string") {
+    message = error;
+  } else {
+    message = "Something went wrong";
+  }
+  return message;
+};
