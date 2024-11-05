80ec66a14871115ee1e7c5c92be307543fbb87bc
LilDre7 <alvaroaburto71@gmail.com>
2024-11-05 11:37:53 -0600

    :zap: Improve performace

--- lib/data.ts
+++ lib/data.ts
@@ -4,7 +4,8 @@ import { LuGraduationCap } from "react-icons/lu";
 import { FaAddressBook } from "react-icons/fa";
 import aifit from "@/public/aifit.jpg";
 import shorlink from "@/public/shortlink.jpg";
-// import gameCart from "";
+import ai from "@/public/gemini.webp";
+import sass from "@/public/saas.webp";
 
 export const links = [
   {
@@ -35,20 +36,28 @@ export const links = [
 
 export const experiencesData = [
   {
-    title: "Graduado Bootcamp Academlo",
-    location: "Mexico, Cuidad de Mexico",
-    description:
-      "Estudié alrededor de 9 meses en un bootcamp de programación para ser desarrollador Web full-stack",
-    icon: React.createElement(LuGraduationCap),
-    date: "2023",
-  },
-  {
     title: "Estudiante de Platzi, Youtube, Open Bootcamp, freeCodeCamp",
     location: "Costa Rica, Latam ",
     description:
       "Estudié alrededor de 9 meses de manera autodidacta con cursos de programación, videos, proyectos, etc. Esto me añadió mucho valor para empezar con mis proyectos FreeLancer.",
     icon: React.createElement(FaAddressBook),
-    date: "2022 - 2023",
+    date: "2022 - 2024",
+  },
+  {
+    title: "Desarrollo Web Full-Stack y Ciencias de Computación",
+    location: "Tecnico superior",
+    description:
+      "Mi conocimiento en ciencias de la computación también me permite resolver problemas de manera efectiva, trabajar con algoritmos y estructuras de datos eficientes y comprender cómo optimizar los sistemas.",
+    icon: React.createElement(CgWorkAlt),
+    date: "2022",
+  },
+  {
+    title: "Graduado Bootcamp(Desarrollo de Software) Academlo",
+    location: "Mexico, Cuidad de Mexico",
+    description:
+      "Estudié alrededor de 9 meses en un bootcamp de programación para ser desarrollador Web full-stack",
+    icon: React.createElement(LuGraduationCap),
+    date: "2023",
   },
   {
     title: "Proyecto freelance",
@@ -73,18 +82,26 @@ export const projectsData = [
     title: "SHORT LINK APP",
     description:
       "ShortURL es una herramienta gratuita para acortar URL y generar enlaces cortos el acortador de URL permite crear un enlace acortado que facilita compartirlo.",
-    tags: ["Full-Stack","Next.js","Prisma", "postgress",],
+    tags: ["Full-Stack", "Next.js", "Prisma", "postgress"],
     imageUrl: shorlink,
     href: "https://getyourlink.vercel.app/",
   },
-  // {
-  //   title: "Ecommerce tecnoPhone",
-  //   description:
-  //     "Un E-commerce Desarrollado en Aproximadamente 48 Horas con Funcionalidades de Inicio de Sesión, Compras y Carrito. Aquí Exhibo Mi Destreza en el Front-End.",
-  //   tags: ["React", "API", "REDUX", "Tailwind", "REACT TOOKITS"],
-  //   imageUrl: "",
-  //   href: "https://lilecommerce.netlify.app/"
-  // },
+  {
+    title: "Gemini Clone",
+    description:
+      "Se trata del  modelo de lenguaje más avanzado de Google hasta el momento y el proyecto de investigación más grande en la historia de la compañía.",
+    tags: ["NextJS", "API", "META", "Tailwind", "SKD"],
+    imageUrl: ai,
+    href: "https://gemini.vercel.ai/",
+  },
+  {
+    title: "Gemini Clone",
+    description:
+      "Saas UI es una biblioteca de componentes React que no interfiere con tu proceso y te ayuda a crear productos SaaS intuitivos con rapidez.",
+    tags: ["NextJS", "chakra", "UI", "Tailwind", "SAAS"],
+    imageUrl: sass,
+    href: "https://saas-ui-nextjs-landing-page.netlify.app/",
+  },
 ] as const;
 
 export const skillsData = [
@@ -107,4 +124,6 @@ export const skillsData = [
   "Python",
   "VueJs",
   "Framer Motion",
+  "Wordpress",
+  "NextJs"
 ] as const;
