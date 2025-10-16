06f5292a2ed2fdd88b789f8c69ca3f884a26c4ae
Alvaro Aburto <alvaroaburto71@gmail.com>
2025-10-16 17:35:53 -0600

    Nuevo archivo

--- components/about.tsx
+++ components/about.tsx
@@ -21,19 +21,10 @@ export default function About() {
       <SectionHeading><span className="aboutHero">Sobre Mi</span></SectionHeading>
       <div className="space-y-4 text-gray-900 dark:text-white">
         <p className="mb-3 mx-3 sm:mx-0 text-gray-500 dark:text-gray-400 first-line:uppercase first-line:tracking-widest first-letter:text-7xl first-letter:font-bold first-letter:text-gray-900 dark:first-letter:text-gray-100 first-letter:me-3 first-letter:float-start">
-          Hola 🙋🏽 , soy Álvaro aunque algunos me conocen como Lil Dree (sí,
-          suena más a rapero que a programador, lo sé). Mi historia con la
-          programación comenzó el día que descubrí que HTML no era una
-          enfermedad. Desde entonces, me paso los días hablando con JavaScript y
-          tratando de domar a React, Node.js y sus amigos. Uno de mis
-          "superpoderes" es hacer funcionar el código a las 3 a.m., después de
-          varios intentos fallidos y una buena dosis de cafeína ☕. He trabajado
-          en proyectos que, al menos, no han explotado... todavía.{" "}
+          Hola, soy Álvaro. Un dev Web & Móvil.
         </p>
         <p className="text-gray-500 dark:text-gray-400">
-          Pero hablando en serio, he desarrollado apps que han ayudado a mejorar
-          la eficiencia de pequeñas empresas, y eso me llena de orgullo. Mi
-          misión en el mundo tech es crear cosas geniales sin perder la cabeza..
+          Me apasiona la tecnología y el desarrollo web. Me gusta crear cosas que sean útiles y que ayuden a la gente.
         </p>
       </div>
     </motion.section>
